79a40ab7f2eefcf471562a5059635e3ab83830b1
unknown <guptarashi578@gmail.com>
2021-07-30 23:52:00 +0530

    card

--- src/component/Basics/Restaurant.js
+++ src/component/Basics/Restaurant.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import Menu from "./MenuApi";
 import MenuCard from './MenuCard';
+import MenuApi from './MenuApi';
 
 const Restaurant = () => {
     const [menuData,setMenuData] = useState(Menu);
     return (
          <>
          <MenuCard/>
+         <MenuApi/>
          </>
     )
 }
